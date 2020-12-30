77ac036a5fc78beb146e5f1e21b24a956a6cccd5
Przemek Nosek <przemo99.10@gmail.com>
2020-12-30 21:25:04 +0100

    Rysowanie figur 2D, Rectangle, Ellipse

--- src/pl/java/swing/poczatek/Main.java
+++ src/pl/java/swing/poczatek/Main.java
@@ -2,6 +2,10 @@ package pl.java.swing.poczatek;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 
 public class Main {
     public static void main(String[] args) {
@@ -27,16 +31,25 @@ public class Main {
 
         // NOT HELLO WORLD PROGRAM
 
+/*
         EventQueue.invokeLater(() -> {
             JFrame frame = new NotHelloWorldFrame();
             frame.setTitle("NOT HELLO WORLD");
             frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
             frame.setVisible(true);
         });
+*/
 
+        EventQueue.invokeLater(() -> {
+            JFrame frame = new DrawFrame();
+            frame.setVisible(true);
+            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            frame.setTitle("JAVA FIGURY");
+        });
     }
 }
 
+// --------------------------------------------------------------------------------- //
 class SimpleFrame extends JFrame {
     private static final int DEFAULT_WIDTH = 300;
     private static final int DEFAULT_HEIGHT = 200;
@@ -46,7 +59,7 @@ class SimpleFrame extends JFrame {
     }
 }
 
-
+// --------------------------------------------------------------------------------- //
 class SizedFrame extends JFrame {
     public SizedFrame() {
         // Sprawdzenie wymiarow ekranu
@@ -68,6 +81,8 @@ class SizedFrame extends JFrame {
     }
 }
 
+
+// --------------------------------------------------------------------------------- //
 // Ramka zawierajaca okienko z komunikatem
 class NotHelloWorldFrame extends JFrame {
     public NotHelloWorldFrame() {
@@ -105,6 +120,64 @@ class NotHelloWorldComponent extends JComponent {
         int screenHeight = screenSize.height;
 
         // Zwracam rozmiary ramki dzielone na 2
-        return new Dimension(screenWidth/2,screenHeight/2);
+        return new Dimension(screenWidth / 2, screenHeight / 2);
+    }
+}
+
+// RYSOWANIE FIGUR
+
+class DrawFrame extends JFrame {
+    public DrawFrame() {
+        add(new DrawComponent());
+        setLocationByPlatform(true);
+        pack();
+    }
+}
+
+class DrawComponent extends JComponent {
+    private static final int DEFAULT_WIDTH = 400;
+    private static final int DEFAULT_HEIGHT = 400;
+
+    public void paint(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+
+        // Rysowanie prostokata
+        double leftX = 100;
+        double topY = 100;
+        double width = 200;
+        double height = 150;
+
+        Rectangle2D rectangle2D = new Rectangle2D.Double(leftX, topY, width, height);
+        g2.draw(rectangle2D);
+
+        // Rysowanie elipsy
+        Ellipse2D ellipse2D = new Ellipse2D.Double();
+        ellipse2D.setFrame(rectangle2D);
+        g2.draw(ellipse2D);
+
+        // Rysowanie przekatnej
+        g2.draw(new Line2D.Double(leftX, topY, leftX + width, topY + height));
+
+        // Rysowanie kola z takim samym srodkiem
+        //Point2D point2D = new Point2D.Double(rectangle2D.getCenterX(), rectangle2D.getCenterY());
+        double centerX = rectangle2D.getCenterX();
+        double centerY = rectangle2D.getCenterY();
+        double radius = 150;
+        Ellipse2D circle = new Ellipse2D.Double();
+        circle.setFrameFromCenter(centerX, centerY, centerX + radius, centerY + radius);
+        g2.draw(circle);
+
+        // Rysowanie punktow co 2 za pomoca drawLine
+        for (int i = 0; i < 400; i++) {
+            if (i % 2 == 0) {
+                g2.drawLine(i, i, i, i);
+            }
+        }
+
+        g2.drawString("Tu jest srodek", DEFAULT_HEIGHT/2, DEFAULT_WIDTH/2);
+    }
+
+    public Dimension getPreferredSize() {
+        return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
     }
 }
