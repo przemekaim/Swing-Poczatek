56a2f3fcd910843e4d152e6dda2e5489a2c2c935
Przemek Nosek <przemo99.10@gmail.com>
2020-12-30 14:20:57 +0100

    Toolkit, Dimension, ImageIcon

--- src/pl/java/swing/poczatek/Main.java
+++ src/pl/java/swing/poczatek/Main.java
@@ -5,14 +5,27 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
+        // SimpleFrame
         // watek dystrybucji zdarzen -> na razie zagadka (Rozdzial 14)
+        /*
         EventQueue.invokeLater(() -> {
             SimpleFrame frame = new SimpleFrame(); // Klasa dziedziczaca po JFrame rozni sie tylko wymiarami okna
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Zakoczenenie programu na zamkniecie ramki
-            frame.setVisible(true); // Ustawiamy widocznosc okna
-        });
-    }
 
+            frame.setVisible(true); // Ustawiamy widocznosc okna
+        });*/
+
+        // SizedFrame
+
+        EventQueue.invokeLater(() -> {
+            JFrame frame = new SizedFrame();
+            frame.setTitle("JAVA TEST");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+
+        });
+
+    }
 }
 
 class SimpleFrame extends JFrame {
@@ -21,5 +34,27 @@ class SimpleFrame extends JFrame {
 
     public SimpleFrame() {
         setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT); // Ustawienie rozmiaru okna
+    }
+}
+
+
+class SizedFrame extends JFrame {
+    public SizedFrame() {
+        // Sprawdzenie wymiarow ekranu
+
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = kit.getScreenSize();
+        int screenHeight = screenSize.height;
+        int screenWidth = screenSize.width;
+
+        // Ustawienie szerokosci i wysokosci ramki oraz polecenie systemowi by ustalil jej polozenie
+        setSize(screenWidth/2, screenHeight/2);
+        setLocationByPlatform(true);
+
+        // Ustawienie ikony
+        Image img = new ImageIcon("icon.gif").getImage();
+        //Image img = new ImageIcon("kaguya.jpg").getImage();
+        setIconImage(img);
+
     }
 }
